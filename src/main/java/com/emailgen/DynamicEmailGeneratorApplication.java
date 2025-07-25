05963b216bcd248c978a0da3a23b5b74b0aa6c74
Sunay Hashimov <sunay.hashimov@mentormate.com>
2025-07-25 15:47:51 +0300

    initial setup

---
+++ src/main/java/com/emailgen/DynamicEmailGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.emailgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicEmailGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicEmailGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emailgen/DynamicEmailGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicEmailGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
