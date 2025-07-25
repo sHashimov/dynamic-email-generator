deee6cb03e76dd071df4e91f27856c4357871ae4
Sunay Hashimov <sunay.hashimov@mentormate.com>
2025-07-25 16:10:56 +0300

    DEG-2 Add /generate-email endpoint with expression stub (Lombok)

--- src/main/java/com/emailgen/controller/EmailGeneratorController.java
+++ src/main/java/com/emailgen/controller/EmailGeneratorController.java
@@ -1,8 +1,22 @@
 package com.emailgen.controller;
 
-import org.springframework.stereotype.Controller;
+import com.emailgen.dto.EmailResponseDTO;
+import com.emailgen.service.EmailGeneratorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Map;
+
+@RestController
+@RequestMapping("/generate-email")
+@RequiredArgsConstructor
 public class EmailGeneratorController {
 
+    private final EmailGeneratorService service;
+
+    @GetMapping
+    public EmailResponseDTO generateEmail(@RequestParam Map<String, String> allParams) {
+        String expression = allParams.remove("expression");
+        return service.generateEmail(allParams, expression);
+    }
 }
---
+++ src/main/java/com/emailgen/dto/EmailResultItem.java
@@ -0,0 +1,11 @@
+package com.emailgen.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EmailResultItem {
+    private String id;
+    private String value;
+}
