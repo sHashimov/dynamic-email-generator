b3f45f669a9553181f1278a624f9a974404378e1
Sunay Hashimov <sunay.hashimov@mentormate.com>
2025-07-28 16:44:52 +0300

    GEN-8 additional config changes

--- src/main/java/com/emailgen/config/OpenApiConfig.java
+++ src/main/java/com/emailgen/config/OpenApiConfig.java
@@ -2,16 +2,12 @@ package com.emailgen.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
-    @Value("${swagger.enabled:true}")
-    private boolean swaggerEnabled;
 
     @Bean
     @ConditionalOnProperty(name = "swagger.enabled", havingValue = "true", matchIfMissing = true)
